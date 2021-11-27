a55cd0eb61730d84300ebbd37f958de57aeb2a03
TeodoraTrifonova <nanamiandtomoe@gmail.com>
2021-11-27 18:21:55 +0200

    yay rabotiiii

--- javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/HelloController.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/HelloController.java
@@ -1,14 +1,31 @@
 package bg.tu_varna.sit.library.presentation.controllers;
 
+import bg.tu_varna.sit.library.presentation.models.HelloModel;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
-public class HelloController {
+public class HelloController implements EventHandler<MouseEvent> {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button helloButton;
+
+    private final HelloModel model;
+
+    public HelloController() {
+        this.model = new HelloModel();
+    }
+    @FXML
+    private void initialize()
+    {
+        helloButton.setOnMouseClicked(this::handle);
+    }
+
+    @Override
+    public void handle(MouseEvent mouseEvent) {
+    welcomeText.setText(model.getWelcomeMessage());
     }
 }
---
+++ javafx/src/main/java/bg/tu_varna/sit/library/presentation/models/HelloModel.java
@@ -0,0 +1,14 @@
+package bg.tu_varna.sit.library.presentation.models;
+
+public class HelloModel {
+
+    private final String welcomeMessage;
+    public  HelloModel ()
+    {
+        this.welcomeMessage="Hello SIT I am Javafx Project";
+    }
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+}
