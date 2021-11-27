1af2bcd69f5ecd4b57726e331112d3f979c49739
TeodoraTrifonova <nanamiandtomoe@gmail.com>
2021-11-27 18:59:55 +0200

    yay rabotiiii

--- javafx/src/main/java/bg/tu_varna/sit/library/common/Constants.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/common/Constants.java
@@ -1,8 +1,7 @@
 package bg.tu_varna.sit.library.common;
 
 public class Constants {
-    public static class View
-    {
+    public static class View {
      public static final String HELLO_VIEW="/bg/tu_varna/sit/library/presentation.view/hello-view.fxml";
     }
 }
