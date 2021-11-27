b983719a445648d84ddff6c1748f11dbd490dd9c
TeodoraTrifonova <nanamiandtomoe@gmail.com>
2021-11-27 20:36:31 +0200

    yay rabotiiii

--- javafx/src/main/java/bg/tu_varna/sit/library/application/HelloApplication.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/application/HelloApplication.java
@@ -1,28 +1,49 @@
 package bg.tu_varna.sit.library.application;
 
 import bg.tu_varna.sit.library.common.Constants;
+import eu.hansolo.tilesfx.Section;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
 
     private static Logger log = Logger.getLogger(HelloApplication.class);
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(Constants.View.HELLO_VIEW));
-         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
 
-        PropertyConfigurator.configure(HelloApplication.class.getResource());
+        PropertyConfigurator.configure(HelloApplication.class.getResource(Constants.Configurations.LOG4J_PROPERTIES));
+        URL path = getClass().getResource(Constants.View.HELLO_VIEW);
 
+        if(path !=null)
+        {
+            Parent root =FXMLLoader.load(path);
+            Scene scene = new Scene(root);
+            scene.setFill(Color.TRANSPARENT);
+
+            stage.setTitle(Constants.Values.Title);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setMaxWidth(780);
+            stage.setMaxHeight(630);
+            stage.setWidth(320);
+            stage.setHeight(240);
+            stage.show();
+        }
+        else
+        {
+            log.error("Sorry, the main fxml could not be loaded");
+            System.exit(-1);
+        }
     }
 
 
--- javafx/src/main/java/bg/tu_varna/sit/library/common/Constants.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/common/Constants.java
@@ -6,6 +6,11 @@ public class Constants {
     }
 
     public static class Configurations{
-        public static final String HELLO_VIEW = "/bg/tu_varna/sit/library/presentation.views/hello-view.fxml";
+        public static final String LOG4J_PROPERTIES = "/bg/tu_varna/sit/library/configuration/log4j.properties";
     }
+
+    public static class Values{
+        public static final String Title = "Example app";
+    }
+
 }
