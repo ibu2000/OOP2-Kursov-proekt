d1c71d3c65a13128d3032503614ef8e007f7a627
ibu2000 <bezvuzvratno@gmail.com>
2021-11-28 15:10:54 +0200

    .

--- javafx/src/main/java/bg/tu_varna/sit/library/data/entities/Task.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/data/entities/Task.java
@@ -1,4 +1,5 @@
 package bg.tu_varna.sit.library.data.entities;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
@@ -10,5 +11,58 @@ public class Task implements Serializable
 
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "userId", nullable = false)
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                '}';
+    }
 }
--- javafx/src/main/java/bg/tu_varna/sit/library/data/entities/User.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/data/entities/User.java
@@ -1,4 +1,9 @@
 package bg.tu_varna.sit.library.data.entities;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Table(name = "tasks")
+@Entity
 public class User {
 }
