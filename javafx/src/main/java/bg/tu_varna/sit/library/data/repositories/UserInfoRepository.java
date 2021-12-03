afcdb795cd76b439086823b6b1cc66691ac69d5e
ibu2000 <bezvuzvratno@gmail.com>
2021-12-03 13:36:40 +0200

    UserInfoRepository save delete update

--- javafx/src/main/java/bg/tu_varna/sit/library/data/repositories/UserInfoRepository.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/data/repositories/UserInfoRepository.java
@@ -1,8 +1,11 @@
 package bg.tu_varna.sit.library.data.repositories;
 
+import bg.tu_varna.sit.library.data.access.Connection;
 import bg.tu_varna.sit.library.data.entities.USER;
 import bg.tu_varna.sit.library.data.entities.UserInfo;
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,17 +22,52 @@ public class UserInfoRepository implements DAORepository<UserInfo> {
 
     @Override
     public void save(UserInfo obj) {
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.save(obj);
+            log.info("User Info saved successfully");
+        }catch (Exception e)
+        {
+            log.error("User Info save error" + e.getMessage());
+        }finally {
+            transaction.commit();
+            session.close();
+        }
 
     }
 
     @Override
     public void update(UserInfo  obj) {
-
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.update(obj);
+            log.info("User Info updated successfully");
+        }catch (Exception e)
+        {
+            log.error("User Info update error" + e.getMessage());
+        }finally {
+            transaction.commit();
+            session.close();
+        }
     }
 
     @Override
     public void delete(UserInfo  obj) {
-
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.delete(obj);
+            log.info("User Info deleted successfully");
+        }catch (Exception e)
+        {
+            log.error("User Info delete error" + e.getMessage());
+        }finally
+        {
+            transaction.commit();
+            session.close();
+        }
     }
 
     @Override
