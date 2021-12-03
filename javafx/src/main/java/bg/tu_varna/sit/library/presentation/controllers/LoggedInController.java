d2b8ab27d3aa534e41965b3a3a5c829f852db2b3
ibu2000 <bezvuzvratno@gmail.com>
2021-12-03 16:14:14 +0200

    UserTypeRepository save delete update

--- javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/LoggedInController.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/LoggedInController.java
@@ -1,4 +1,37 @@
 package bg.tu_varna.sit.library.presentation.controllers;
 
-public class LoggedInController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoggedInController implements Initializable {
+
+    @FXML
+    private Button button_logout;
+    @FXML
+    private Label label_welcome;
+    @FXML
+    private Label label_fav_channel;
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+        button_logout.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle (ActionEvent event) {
+            //DBUtils.changeScene(event, "sample.fxml", "log in!", null, null);
+
+            }
+        });
+    }
+
+    public void setUserInformation(String username, String favChannel){
+        label_welcome.setText("Welcome "+username+"?");
+        label_fav_channel.setText("Your favorite YouTube channel is " + favChannel +"?");
+    }
 }
