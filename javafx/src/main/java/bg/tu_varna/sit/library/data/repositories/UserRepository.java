73acb3d3256abd27a66fecce82f81b97f02a8a03
ibu2000 <bezvuzvratno@gmail.com>
2021-12-03 13:38:25 +0200

    UserInfoRepository save delete update

--- javafx/src/main/java/bg/tu_varna/sit/library/data/repositories/UserRepository.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/data/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package bg.tu_varna.sit.library.data.repositories;
 
+import bg.tu_varna.sit.library.data.access.Connection;
 import bg.tu_varna.sit.library.data.entities.USER;
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,17 +21,51 @@ public class UserRepository implements DAORepository<USER> {
 
     @Override
     public void save(USER obj) {
-
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.save(obj);
+            log.info("User saved successfully");
+        }catch (Exception e)
+        {
+            log.error("User save error" + e.getMessage());
+        }finally {
+            transaction.commit();
+            session.close();
+        }
     }
 
     @Override
     public void update(USER obj) {
-
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.update(obj);
+            log.info("User updated successfully");
+        }catch (Exception e)
+        {
+            log.error("User update error" + e.getMessage());
+        }finally {
+            transaction.commit();
+            session.close();
+        }
     }
 
     @Override
     public void delete(USER obj) {
-
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.delete(obj);
+            log.info("User deleted successfully");
+        }catch (Exception e)
+        {
+            log.error("User delete error" + e.getMessage());
+        }finally
+        {
+            transaction.commit();
+            session.close();
+        }
     }
 
     @Override
