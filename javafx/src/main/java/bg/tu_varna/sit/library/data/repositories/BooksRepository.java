aee78cf1d2d67977f30c7db3660c3e22923792f3
ibu2000 <bezvuzvratno@gmail.com>
2021-12-03 13:10:56 +0200

    BookRepository save delete update

--- javafx/src/main/java/bg/tu_varna/sit/library/data/repositories/BooksRepository.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/data/repositories/BooksRepository.java
@@ -1,6 +1,9 @@
 package bg.tu_varna.sit.library.data.repositories;
+import bg.tu_varna.sit.library.data.access.Connection;
 import bg.tu_varna.sit.library.data.entities.Books;
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,16 +20,53 @@ public class BooksRepository implements DAORepository<Books>{
 
     @Override
    public void save(Books obj)
-    {
+    {    Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.save(obj);
+            log.info("Book saved successfully");
+            }catch (Exception e)
+        {
+            log.error("Book save error" + e.getMessage());
+        }finally {
+            transaction.commit();
+            session.close();
+        }
     }
+
 
     @Override
     public void update(Books obj) {
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.update(obj);
+            log.info("Product updated successfully");
+            }catch (Exception e)
+            {
+                log.error("Product update error" + e.getMessage());
+            }finally {
+                transaction.commit();
+                session.close();
+        }
     }
 
     @Override
     public void delete(Books obj) {
 
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.delete(obj);
+            log.info("Product deleted successfully");
+            }catch (Exception e)
+            {
+                log.error("Product delete error" + e.getMessage());
+            }finally
+            {
+                transaction.commit();
+                session.close();
+            }
     }
 
     @Override
