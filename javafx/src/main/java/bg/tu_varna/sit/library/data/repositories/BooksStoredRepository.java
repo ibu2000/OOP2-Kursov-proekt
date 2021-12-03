4d4f3b87ecde3853178d82707183a1535afa28af
ibu2000 <bezvuzvratno@gmail.com>
2021-12-03 13:18:35 +0200

    BooksStored save delete update 1

--- javafx/src/main/java/bg/tu_varna/sit/library/data/repositories/BooksStoredRepository.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/data/repositories/BooksStoredRepository.java
@@ -1,8 +1,11 @@
 package bg.tu_varna.sit.library.data.repositories;
 
+import bg.tu_varna.sit.library.data.access.Connection;
 import bg.tu_varna.sit.library.data.entities.BOOKSSTORED;
 import bg.tu_varna.sit.library.data.entities.Eksemplqri;
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,17 +21,53 @@ public class BooksStoredRepository implements DAORepository<BOOKSSTORED> {
 
     @Override
     public void save(BOOKSSTORED obj) {
-
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.save(obj);
+            log.info("Books stored saved successfully");
+        }catch (Exception e)
+        {
+            log.error("Books stored save error" + e.getMessage());
+        }finally {
+            transaction.commit();
+            session.close();
+        }
     }
 
     @Override
     public void update(BOOKSSTORED obj) {
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.update(obj);
+            log.info("Books Stored updated successfully");
+        }catch (Exception e)
+        {
+            log.error("Books Stored update error" + e.getMessage());
+        }finally {
+            transaction.commit();
+            session.close();
+        }
 
     }
 
     @Override
     public void delete(BOOKSSTORED obj) {
 
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.delete(obj);
+            log.info("Books Stored deleted successfully");
+        }catch (Exception e)
+        {
+            log.error("Books Stored delete error" + e.getMessage());
+        }finally
+        {
+            transaction.commit();
+            session.close();
+        }
     }
 
     @Override
