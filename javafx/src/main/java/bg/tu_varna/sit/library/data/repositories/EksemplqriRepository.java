fb11fbfcaa98c2f4abb3c1f89693454b20074829
ibu2000 <bezvuzvratno@gmail.com>
2021-12-03 13:22:55 +0200

    EksemplqriRepository save delete update 1

--- javafx/src/main/java/bg/tu_varna/sit/library/data/repositories/EksemplqriRepository.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/data/repositories/EksemplqriRepository.java
@@ -1,8 +1,11 @@
 package bg.tu_varna.sit.library.data.repositories;
 
+import bg.tu_varna.sit.library.data.access.Connection;
 import bg.tu_varna.sit.library.data.entities.Eksemplqri;
 import bg.tu_varna.sit.library.data.entities.LENDINFO;
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,15 +22,51 @@ public class EksemplqriRepository implements DAORepository<Eksemplqri> {
     @Override
     public void save(Eksemplqri obj) {
 
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.save(obj);
+            log.info("Eksemplqri saved successfully");
+        }catch (Exception e)
+        {
+            log.error("Eksemplqri save error" + e.getMessage());
+        }finally {
+            transaction.commit();
+            session.close();
+        }
     }
 
     @Override
     public void update(Eksemplqri obj) {
-
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.update(obj);
+            log.info("Eksemplqri updated successfully");
+        }catch (Exception e)
+        {
+            log.error("Eksemplqri update error" + e.getMessage());
+        }finally {
+            transaction.commit();
+            session.close();
+        }
     }
 
     @Override
     public void delete(Eksemplqri obj) {
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.delete(obj);
+            log.info("Eksemplqri deleted successfully");
+        }catch (Exception e)
+        {
+            log.error("Eksemplqri delete error" + e.getMessage());
+        }finally
+        {
+            transaction.commit();
+            session.close();
+        }
 
     }
 
