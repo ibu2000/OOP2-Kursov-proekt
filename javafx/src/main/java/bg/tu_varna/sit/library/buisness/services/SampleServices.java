37cae010a88a1ce030ef6877d6a90481f773cf44
TeodoraTrifonova <92271887+TeodoraTrifonova@users.noreply.github.com>
2021-12-05 19:07:54 +0200

    it works !

---
+++ javafx/src/main/java/bg/tu_varna/sit/library/buisness/services/SampleServices.java
@@ -0,0 +1,13 @@
+package bg.tu_varna.sit.library.buisness.services;
+
+import bg.tu_varna.sit.library.data.repositories.StatusRepository;
+
+public class SampleServices {
+
+   /* public static SampleServices getInstance()
+    {
+        return StatusRepository.S;
+    }
+
+  */
+}
--- javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/SampleController.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/SampleController.java
@@ -1,4 +1,21 @@
 package bg.tu_varna.sit.library.presentation.controllers;
 
+import bg.tu_varna.sit.library.buisness.services.SampleServices;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class SampleController {
+    Stage s;
+
+    public SampleController(Stage stage)
+    {
+        s=stage;
+    }
+
+//    private final SampleServices sampleServices = SampleServices.getInstance();
+
+    @FXML
+   private Button button_log_in;
+
 }
