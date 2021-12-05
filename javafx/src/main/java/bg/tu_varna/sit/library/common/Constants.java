eeeb8571148f230457309501884e4a0e777a760c
TeodoraTrifonova <92271887+TeodoraTrifonova@users.noreply.github.com>
2021-12-05 23:19:25 +0200

    controller

--- javafx/src/main/java/bg/tu_varna/sit/library/common/Constants.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/common/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     }
 
     public static class Values{
-        public static final String Title = "Example app";
+        public static final String Title = "White Orchard Library";
     }
 
 }
