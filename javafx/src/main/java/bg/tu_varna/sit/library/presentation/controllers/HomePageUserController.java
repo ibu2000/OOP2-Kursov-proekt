6df3dc798d73f8f1f54da44302de10a20e434252
TeodoraTrifonova <92271887+TeodoraTrifonova@users.noreply.github.com>
2021-12-11 16:48:35 +0200

    controller

--- javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/HomePageUserController.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/HomePageUserController.java
@@ -1,4 +1,14 @@
 package bg.tu_varna.sit.library.presentation.controllers;
 
+import javafx.stage.Stage;
+
 public class HomePageUserController {
+
+
+    public HomePageUserController()
+    {}
+    public HomePageUserController(Stage stage)
+    {
+      
+    }
 }
--- javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/SampleController.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/SampleController.java
@@ -4,6 +4,8 @@ import bg.tu_varna.sit.library.common.Constants;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -29,9 +31,9 @@ public class SampleController {
         try
         {
             s.close();
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Constants.LogginPage.LOGGINPAGE));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Constants.View.HOMEPAGE_USER));
             Stage stage = new Stage();
-            fxmlLoader.setController(new MolController(stage));
+            fxmlLoader.setController(new HomePageUserController(stage));
             Parent root2 = fxmlLoader.load();
             stage.setScene(new Scene(root2));
             stage.show();
