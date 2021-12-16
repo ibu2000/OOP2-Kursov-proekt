8691264c173a54d2a49101c674b407ebc35c4258
TeodoraTrifonova <92271887+TeodoraTrifonova@users.noreply.github.com>
2021-12-16 15:45:16 +0200

    Create 123.sql

---
+++ mysql/123.sql
@@ -0,0 +1,21 @@
+INSERT INTO status (idStatus, status)VALUES (1, 's1');
+INSERT INTO status (idStatus, status)VALUES (2, 's2');
+INSERT INTO status (idStatus, status)VALUES (3, 's3');
+ 
+INSERT INTO usertype (idUserType, UserType)VALUES (1, 'type1');
+INSERT INTO usertype (idUserType, UserType)VALUES (2, 'type2');
+INSERT INTO usertype (idUserType, UserType)VALUES (3, 'type3');
+
+
+INSERT INTO books (idBook, bookName, author, genre, yearOfPublishing)VALUES (1, 'bName1', 'author1', 'genre1', '2008-7-04');
+INSERT INTO books (idBook, bookName, author, genre, yearOfPublishing)VALUES (2, 'bName2', 'author2', 'genre2', '2009-8-01');
+INSERT INTO books (idBook, bookName, author, genre, yearOfPublishing)VALUES (3, 'bName3', 'author3', 'genre3', '2017-7-12');
+ 
+INSERT INTO user (idUser, userName, password, dateOfUserApproval, rating,USERTYPE_idUserType,STATUS_idStatus)VALUES (1, 'u1', 'pas1','2017-7-12', 'good', 1,1);
+INSERT INTO user (idUser, userName, password, dateOfUserApproval, rating,USERTYPE_idUserType,STATUS_idStatus)VALUES (2, 'u2', 'pas2','2012-5-10', 'good', 2,2);
+INSERT INTO user (idUser, userName, password, dateOfUserApproval, rating,USERTYPE_idUserType,STATUS_idStatus)VALUES (3, 'u3', 'pas3','2018-1-11', 'good', 3,3);
+ 
+
+
+ 
+ INSERT INTO userinfo (idUser, name, phone, email)VALUES (1, 'name1', 'phone1', 'email1');
