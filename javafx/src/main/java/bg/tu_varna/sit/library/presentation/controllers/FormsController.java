be2d5ceda5cff7dd03bb08649c85752cd932f06a
ibu2000 <bezvuzvratno@gmail.com>
2022-01-21 16:52:10 +0200

    forms

--- javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/FormsController.java
+++ javafx/src/main/java/bg/tu_varna/sit/library/presentation/controllers/FormsController.java
@@ -1,18 +1,42 @@
 package bg.tu_varna.sit.library.presentation.controllers;
 
 import bg.tu_varna.sit.library.common.Constants;
+import bg.tu_varna.sit.library.data.entities.Books;
+import bg.tu_varna.sit.library.data.entities.USER;
+import bg.tu_varna.sit.library.data.entities.UserType;
+import bg.tu_varna.sit.library.presentation.models.FormModel;
+import bg.tu_varna.sit.library.presentation.models.UserListModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+
+import java.time.LocalDate;
 
 public class FormsController {
     @FXML
-    private Button buttonF_forms;
+    private Button pending;
+    @FXML
+    private Button notPending;
     @FXML
     private Button buttonF_home;
+
+    @FXML
+    private TableView<FormModel> form;
+    @FXML
+    private TableColumn<UserListModel, Long> idForm;
+    @FXML
+    private TableColumn<UserListModel, LocalDate> submitionDate;
+    @FXML
+    private TableColumn<UserListModel, String> content;
+    @FXML
+    private TableColumn<UserListModel, USER> idUser;
+    @FXML
+    private TableColumn<UserListModel, String> status;
 
     Stage s;
 
